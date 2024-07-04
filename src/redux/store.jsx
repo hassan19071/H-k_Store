79644a0a6a3b4717ce41ff25b2c25253c91c1483
unknown <https://github.com/hassan19071>
2024-07-04 23:26:15 +0300

    change userData name

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,18 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import changeSlider  from './data/Slider'
 import filterTheProducts  from './data/FilteringProducts'
-// import  cartSettings  from './data/CartSettings'
-// import  wishlistSettings  from './data/WishlistSettings'
-// import OrderSettings from './data/OrderSettings'
 import userData from './data/UserData'
 
 export const store = configureStore({
   reducer: {
     slider : changeSlider,
     filter : filterTheProducts,
-    // cart : cartSettings,
-    // wishlist: wishlistSettings,
-    // orders: OrderSettings,
     user: userData
   }
 })
